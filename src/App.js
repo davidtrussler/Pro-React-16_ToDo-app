65e6aa0829c8e23cab477da0f205575a24113455
David Trussler <mail@davidtrussler.net>
2020-02-11 06:01:50 +0000

    Adds display of dynamic content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 
 export default class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: 'David'
+    }
+  }
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === 'David' ? 'Jude' : 'David'
+    })
+  }
+
+  render = () => {
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2">
-          To Do List
+          {this.state.userName}'s To Do List
         </h4>
+
+        <button className="btn btn-primary m-2" onClick={this.changeStateData}>
+          Change
+        </button>
       </div>
     )
   }
